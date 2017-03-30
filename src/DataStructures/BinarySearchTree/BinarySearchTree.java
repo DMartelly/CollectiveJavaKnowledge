7ca298f2f011bfae410f1c11277dc83764dc1300
Dominick Martelly <dominick.martelly@gmail.com>
2017-03-30 11:49:56 -0400

    Fixed some null pointers

--- src/DataStructures/BinarySearchTree/BinarySearchTree.java
+++ src/DataStructures/BinarySearchTree/BinarySearchTree.java
@@ -7,86 +7,90 @@ package DataStructures.BinarySearchTree;
  */
 public class BinarySearchTree {
 
-    /**
-     * A Binary Search Tree's node. Holds a value, and two children nodes
-     *
-     * @author Dominick Martelly
-     */
-    private class Node {
+	/**
+	 * A Binary Search Tree's node. Holds a value, and two children nodes
+	 *
+	 * @author Dominick Martelly
+	 */
+	private class Node {
 
-        int value;
-        /**
-         * The node's left child. Should have a lower value than the node
-         */
-        Node left;
-        /**
-         * The node's right child. Should have a greater value than the node
-         */
-        Node right;
+		int value;
+		/**
+		 * The node's left child. Should have a number lower than value
+		 */
+		Node left;
+		/**
+		 * The node's right child. Should have a number greater than value
+		 */
+		Node right;
 
-        /**
-         * Creates a Node holding an integer value
-         *
-         * @param x An integer value
-         */
-        Node(int x) {
-            this.value = x;
-        }
+		/**
+		 * Creates a Node holding an integer value
+		 *
+		 * @param x An integer value
+		 */
+		Node(int x) {
+			this.value = x;
+		}
 
-        /**
-         * Adds a new Node to a tree containing x as it's value
-         *
-         * @param x An integer value
-         */
-        Node add(Node root, int x) {
-            if (root == null) {
-                return new Node(x);
-            }
-            if (x < root.value) {
-                root.left = add(root.left, x);
-            } else {
-                root.right = add(root.right, x);
-            }
-            return root;
-        }
+		/**
+		 * Adds a new Node to a tree containing x as it's value
+		 *
+		 * @param root the current root node
+		 * @param x An integer value
+		 * @return the modified / original node
+		 */
+		private Node add(Node root, int x) {
+			if (root == null) {
+				return new Node(x);
+			}
+			if (x < root.value) {
+				root.left = add(root.left, x);
+			} else {
+				root.right = add(root.right, x);
+			}
+			return root;
+		}
 
-        /**
-         * @param x An integer value
-         * @return The node containing x
-         */
-        private Node find(Node root, int x) {
-            if (root == null)
-                return null;
-            if (root.value == x)
-                return root;
-            if (x < root.value)
-                return find(root.left, x);
-            else
-                return find(root.right, x);
-        }
+		/**
+		 * 
+		 * @param root The current root node
+		 * @param x An integer value
+		 * @return The node containing x
+		 */
+		private Node find(Node root, int x) {
+			if (root == null)
+				return null;
+			if (root.value == x)
+				return root;
+			if (x < root.value)
+				return find(root.left, x);
+			else
+				return find(root.right, x);
+		}
 
-        /**
-         * Removes a Node from a tree
-         *
-         * @param root the tree to use
-         * @param x    An integer value
-         * @return True if the Node was found and deleted
-         */
-        Node delete(Node root, int x) {
-            if (root == null) {
-                return root;
-            }
-            if (root.value == x){
-            	root = recDelete(root);
-            } else if (x < root.value){
-            	root.left = delete(root.left, x);
-            } else {
-            	root.right = delete(root.right, x);
-            }
-            return root;
-        }
+		/**
+		 * Removes a Node from a tree
+		 *
+		 * @param root the tree to use
+		 * @param x    An integer value
+		 * @return True if the Node was found and deleted
+		 */
+		private Node delete(Node root, int x) {
+			if (root == null) {
+				return root;
+			}
+			if (root.value == x){
+				root = recDelete(root);
+			} else if (x < root.value){
+				root.left = delete(root.left, x);
+			} else {
+				root.right = delete(root.right, x);
+			}
+			return root;
+		}
 
-        private Node recDelete(Node toDelete) {
+		private Node recDelete(Node toDelete) {
 			if (toDelete.right == null && toDelete.left != null){
 				return toDelete.left;
 			} else if (toDelete.left == null && toDelete.right != null){
@@ -100,155 +104,128 @@ public class BinarySearchTree {
 		}
 
 		/**
-         * Searches though the tree of the Node for a Node containing the
-         * current value
-         *
-         * @param x An integer value
-         * @return True if a Node containing x was found
-         */
-        boolean search(int x) {
-            if (x == this.value)
-                return true;
-            if (this.isLeaf())
-                return false;
+		 * Checks if the Node has any child Nodes
+		 *
+		 * @return True if Node is a leaf
+		 */
+		private boolean isLeaf() {
+			return (this.left == null && this.right == null);
+		}
 
-            if (x < this.value && this.left != null)
-                return this.left.search(x);
-            else if (x > this.value && this.right != null)
-                return this.right.search(x);
-            else
-                return false;
-        }
+		/**
+		 * @return The largest node of the current tree
+		 */
+		private Node getLargestChild() {
+			while (this.right != null)
+				return (this.right.getLargestChild());
+			return this;
+		}
 
-        /**
-         * Checks if the Node has any child Nodes
-         *
-         * @return True if Node is a leaf
-         */
-        boolean isLeaf() {
-            return (this.left == null && this.right == null);
-        }
+		/**
+		 * @return The smallest node of the current tree
+		 */
+		private Node getSmallestChild() {
+			while (this.left != null)
+				return (this.left.getSmallestChild());
+			return this;
+		}
 
-        /**
-         * @return The int in the node
-         */
-        int getData() {
-            return this.value;
-        }
+		@Override
+		public String toString() {
+			// If there is only one element
+			if (this.isLeaf())
+				return Integer.toString(this.value) + " ";
 
-        /**
-         * @return The largest node of the current tree
-         */
-        Node getLargestChild() {
-            while (this.right != null)
-                return (this.right.getLargestChild());
-            return this;
-        }
+			String listofNodes = "";
 
-        /**
-         * @return The smallest node of the current tree
-         */
-        Node getSmallestChild() {
-            while (this.left != null)
-                return (this.left.getSmallestChild());
-            return this;
-        }
+			if (this.left != null)
+				listofNodes += this.left.toString();
 
-        @Override
-        public String toString() {
-            // If there is only one element
-            if (this.isLeaf())
-                return Integer.toString(this.value) + " ";
+			listofNodes += this.value + " ";
 
-            String listofNodes = "";
+			if (this.right != null)
+				listofNodes += this.right.toString();
 
-            if (this.left != null)
-                listofNodes += this.left.toString();
+			return listofNodes;
+		}
+	}
 
-            listofNodes += this.value + " ";
+	/**
+	 * The root Node of the Binary Search Tree
+	 */
+	private Node root;
 
-            if (this.right != null)
-                listofNodes += this.right.toString();
+	/**
+	 * Creates an empty BST
+	 */
+	BinarySearchTree() {
+		root = null;
+	}
 
-            return listofNodes;
-        }
-    }
+	/**
+	 * Inserts a new node containing x into the tree
+	 *
+	 * @param x - an integer value
+	 */
+	void insert(int x) {
+		if (root == null)
+			root = new Node(x);
+		else
+			root.add(root, x);
+	}
+	
+	/**
+	 * Searches if there is a node containing x within the BST
+	 *
+	 * @param x an integer value
+	 * @return true if the value is found
+	 */
+	boolean search(int x) {
+		if (root == null)
+			return false;
+		return root.find(root, x) != null;
+	}
 
-    /**
-     * The root Node of the Binary Search Tree
-     */
-    private Node root;
+	/**
+	 * Removes a Node from a BST
+	 *
+	 * @param x if the Node contains this element, the Node will be
+	 *          removed
+	 * @return true if removed
+	 */
+	boolean remove(int x) {
+		if (root == null)
+			return false;
+		if (root.find(root, x) != null){
+			root = root.delete(root, x);
+			return true;
+		}
+		return false;
+	}
 
-    /**
-     * Creates an empty BST
-     */
-    BinarySearchTree() {
-        root = null;
-    }
+	/**
+	 * Gets the right most element in the BST
+	 *
+	 * @return the largest integer in the BST
+	 */
+	int getLargest() {
+		return root.getLargestChild().value;
+	}
 
-    /**
-     * Inserts a new node containing x into the tree
-     *
-     * @param x - an integer value
-     */
-    void insert(int x) {
-        if (root == null)
-            root = new Node(x);
-        else
-            root.add(root, x);
-    }
+	/**
+	 * Gets the left most element in the BST
+	 *
+	 * @return the smallest integer in the BST
+	 */
+	int getSmallest() {
+		return root.getSmallestChild().value;
+	}
 
-    /**
-     * Removes a Node from a BST
-     *
-     * @param x if the Node contains this element, the Node will be
-     *          removed
-     * @return true if removed
-     */
-    boolean remove(int x) {
-        if (root.find(root, x) != null){
-        	root = root.delete(root, x);
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * Gets the right most element in the BST
-     *
-     * @return the largest integer in the BST
-     */
-    int getLargest() {
-        return root.getLargestChild().getData();
-    }
-
-    /**
-     * Gets the left most element in the BST
-     *
-     * @return the smallest integer in the BST
-     */
-    int getSmallest() {
-        return root.getSmallestChild().getData();
-    }
-
-    /**
-     * Searches if there is a node containing x within the BST
-     *
-     * @param x an integer value
-     * @return true if the value is found
-     */
-    boolean binarySearch(int x) {
-        if (root == null)
-            return false;
-        else
-            return root.search(x);
-    }
-
-    @Override
-    public String toString() {
-        if (root == null) {
-            return "";
-        }
-        return root.toString();
-    }
+	@Override
+	public String toString() {
+		if (root == null) {
+			return "";
+		}
+		return root.toString();
+	}
 }
